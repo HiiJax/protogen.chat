95b17b59411c878dd232494c2b176805a09b3282
Jax <git@hiijax.net>
2026-03-12 07:20:16 -0400

    write to db attempt

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,13 +5,22 @@ export async function load(context) {
     let boops;
 
     if (!dev) {
-        ps = context.platform.env.PROTOGEN_DB.prepare("SELECT Boops from Boops WHERE ProtoID = 1");
+        ps = context.platform.env.PROTOGEN_DB
+            .prepare("SELECT Boops from Boops WHERE ProtoID = 1");
         boops = await ps.first("Boops");
     } else {
-        boops = 1;
+        boops = "(dev)";
     }
 
     return {
         boops
     };
 }
+
+export const actions = {
+    boop: async (context) => {
+        await context.platform?.env.PROTOGEN_DB
+            .prepare("UPDATE Boops SET count = count + 1 WHERE id = 1")
+            .run();
+    }
+} 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { enhance } from "$app/forms";
     import { onMount } from "svelte";
 
     /**@type {SVGAnimateElement}*/
@@ -28,7 +29,7 @@
     <h1>Welcome, Protogens!</h1>
     
 </div>
-<div class="proto-face-holder flicker-in" >
+<form class="proto-face-holder flicker-in" method="POST" action="?/boop" use:enhance>
     <svg
         onclick={startBoot}
         class="proto-face"
@@ -55,7 +56,7 @@
             style="fill:currentColor;stroke:currentColor;stroke-width:6.29291;stroke-linecap:round;stroke-linejoin:round;"
             d="m 609.37335,397.44539 -15.39148,-13.41786 20.96793,2.36776 z m 61.25331,0 15.39148,-13.41786 -20.96793,2.36776 z"/>
     </svg>
-</div>
+</form>
 <div class="hero">
     <div class="smol">I have been booped {data.boops} times!</div>
 </div>
