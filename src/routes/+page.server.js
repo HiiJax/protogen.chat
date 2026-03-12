296c9863022107bafced5b857d4e756c6f44f07d
Jax <git@hiijax.net>
2026-03-12 07:25:12 -0400

    fix old names

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -20,7 +20,7 @@ export async function load(context) {
 export const actions = {
     boop: async (context) => {
         await context.platform?.env.PROTOGEN_DB
-            .prepare("UPDATE Boops SET count = count + 1 WHERE id = 1")
+            .prepare("UPDATE Boops SET Boops = Boops + 1 WHERE ProtoID = 1")
             .run();
     }
 } 
