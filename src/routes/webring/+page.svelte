018c2cf8732d5f144f232ed656cccb962dd7ed34
Jax <git@hiijax.net>
2026-04-21 06:05:13 -0400

    clean up webring page more

--- src/routes/webring/+page.svelte
+++ src/routes/webring/+page.svelte
@@ -7,125 +7,160 @@
 </svelte:head>
 
 <h1>Protogen Webring</h1>
-<hr>
 <a class="smol" href="https://indieweb.org/webring">First time seeing a webring? </a>
-<p>
-  Within this webring, you'll find websites related to "protogens", a fictional species created by 
-  <a href="https://coolkoinu.carrd.co/">KOINU</a> in the <a href="https://en.wikipedia.org/wiki/Furry_fandom">furry fandom</a>.
-  Members might use them to represent themselves (this is usually referred to as a <a href="https://en.wikipedia.org/wiki/Fursona">fursona</a>), 
-  have created character(s) based on the species, be an artist that draws other's protogens, or maybe they're just a really big fan of protogens!
-</p>
-<p>
-  You can learn more about protogens on <a href="https://zoruniverse.info/">Zenith's Outer Reach</a>.
-</p>
-<h2>Members</h2>
-<p>The following sites are in the ring! We're still brand new and looking for members!</p>
-<table>
-  <thead>
-    <tr>
-      <th>Website</th>
-      <th>NSFW?</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each data.webring as { website, nsfw }}
-    <tr>
-      <td><a href="https://{website}">{website}</a></td>
-      <td>{nsfw}</td>
-    </tr>
-    {/each}
-  </tbody>
-</table>
-<hr>
-<h2>Join the Ring!</h2>
-<p>
-  If you already have a website, it's easy to join the ring! Just add the links to your site and make sure you follow the rules. 
-  Once you're ready, <a href="/contact">send a message</a> or (if you're feeling savvy) 
-  <a href="https://github.com/HiiJax/protogen.chat/blob/main/src/routes/webring/webring.js">add yourself</a> and 
-  <a href="https://github.com/HiiJax/protogen.chat/pulls">open a pull request</a>!
-</p>
-<h3>Links</h3>
-<p>Your site just needs the following links to join the Protogen Webring. No JavaScript needed!</p>
-<table>
-  <thead>
-    <tr>
-      <th>Potential Label</th>
-      <th>href</th>
-    </tr>
-    <tr>
-      <td>ProtogenRing</td>
-      <td><code>https://protogen.chat/webring</code></td>
-    </tr>
-    <tr>
-      <td>&lAarr; Prev</td>
-      <td><code>https://protogen.chat/webring/<i>your.domain</i>/previous</code></td>
-    </tr>
-    <tr>
-      <td>Next &rAarr;</td>
-      <td><code>https://protogen.chat/webring/<i>your.domain</i>/next</code></td>
-    </tr>
-  </thead>
-</table>
-<p>Currently, styling has not yet been created for the links. You can check out how members have implemented the links for inspiration.</p>
-<!--
-<p>The following links are available to use, but not currently required to join the Protogen Webring.</p>
-<table>
-  <thead>
-    <tr>
-      <th>Suggested Label</th>
-      <th>href</th>
-    </tr>
-    <tr>
-      <td>Random</td>
-      <td><code>https://protogen.chat/webring/<i>your.domain</i>/random</code></td>
-    </tr>
-  </thead>
-</table>
--->
-<hr>
-<h2>Rules</h2>
-<div class="rules">
-<p>All rules are subject to change. Enforcement and uptime will be offered on a "best effort" basis.</p>
-<h5>Must be a website</h5>
-<ul>
-  <li>The webring must be able to link <i>to</i> the root of a domain (e.g. your.domain) or subdomain (e.g. yoursubdomain.neocities.org)</li>
-</ul>
-<h5>Must be accessible</h5>
-<ul>
-  <li>The webring links should be in an easy to access place on your website, preferably shown on every page in a header, footer, or sidebar.</li>
-  <li>Paths (example.com/links) are discouraged, but acceptable, for your outgoing links <i>from</i> the webring. The webring can't link <i>to</i> paths.</li>
-</ul>
-<h5>NSFW content MUST be gated</h5>
-<ul>
-  <li>
-    If your website contains ANY kind of 18+ content, you MUST have a warning page prior 
-    to entering the site. The webring MUST be accessible from said page so that users 
-    that do not wish to see the NSFW content can still continue through the ring.
-  </li>
-</ul>
-<h5>Sites must have relevance to protogens</h5>
-<ul>
-  <li>You don't need to <i>be</i> a protogen to join the ring, you don't even need a protogen OC. Protogen artists and fans are all welcome too!</li>
-  <li>As long as you've got a protogen sona or your site has something that protogen enjoyers might like to see, you're welcome to join!</li>
-</ul>
-<h5>Don't be shitty</h5>
-<ul>
-  <li>Hate is not tolerated in the slightest.</li>
-  <li>Keeping furry weird is fine. But being rude, creepy, etc is not acceptable.</li>
-</ul>
+<br>
+<br>
+<div class="service-grid">
+  <div class="service">
+    <div class="service-header">
+      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M8.05 9.6c.336 0 .504-.24.554-.627.04-.534.198-.815.847-1.26.673-.475 1.049-1.09 1.049-1.986 0-1.325-.92-2.227-2.262-2.227-1.02 0-1.792.492-2.1 1.29A1.7 1.7 0 0 0 6 5.48c0 .393.203.64.545.64.272 0 .455-.147.564-.51.158-.592.525-.915 1.074-.915.61 0 1.03.446 1.03 1.084 0 .563-.208.885-.822 1.325-.619.433-.926.914-.926 1.64v.111c0 .428.208.745.585.745"/>
+        <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911z"/>
+        <path d="M7.001 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0"/>
+      </svg>
+      <h2 class="service-name">About</h2>
+    </div>
+    <hr>
+    <p>
+      Within this webring, you'll find websites related to "protogens", a fictional species created by 
+      <a href="https://coolkoinu.carrd.co/">KOINU</a> in the <a href="https://en.wikipedia.org/wiki/Furry_fandom">furry fandom</a>.
+      Members might use them to represent themselves (this is usually referred to as a <a href="https://en.wikipedia.org/wiki/Fursona">fursona</a>), 
+      have created character(s) based on the species, be an artist that draws other's protogens, or maybe they're just a really big fan of protogens!
+    </p>
+    <p>
+      You can learn more about protogens on <a href="https://zoruniverse.info/">Zenith's Outer Reach</a>.
+    </p>
+  </div>
+  <div class="service">
+    <div class="service-header">
+      <svg class="service-icon" viewBox="0 0 67.733334 67.733329" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="m 71.515982,62.969097 c 0,11.337774 -9.191082,20.528856 -20.528857,20.528856 -11.337774,0 -20.528856,-9.191082 -20.528856,-20.528856 0,-11.337774 9.191082,-20.528856 20.528856,-20.528856 11.337775,0 20.528857,9.191082 20.528857,20.528856 z m 46.985938,0 c 0,11.337774 9.19108,20.528856 20.52886,20.528856 11.33777,0 20.52885,-9.191082 20.52885,-20.528856 0,-11.337774 -9.19108,-20.528856 -20.52885,-20.528856 -11.33778,0 -20.52886,9.191082 -20.52886,20.528856 z M 71.515982,209.01563 c 0,-11.33778 -9.191082,-20.52886 -20.528857,-20.52886 -11.337774,0 -20.528856,9.19108 -20.528856,20.52886 0,11.33777 9.191082,20.52885 20.528856,20.52885 11.337775,0 20.528857,-9.19108 20.528857,-20.52885 z m 46.985938,0 c 0,-11.33778 9.19108,-20.52886 20.52886,-20.52886 11.33777,0 20.52885,9.19108 20.52885,20.52886 0,11.33777 -9.19108,20.52885 -20.52885,20.52885 -11.33778,0 -20.52886,-9.19108 -20.52886,-20.52885 z"
+          transform="matrix(0.32220904,0,0,0.32220904,3.2539254,-9.9513033)" />
+        <path
+          d="m 52.157282,107.95235 c 0,10.71503 -8.686251,19.40128 -19.401285,19.40128 -10.715034,0 -19.401285,-8.68625 -19.401285,-19.40128 0,-10.715037 8.686251,-19.401288 19.401285,-19.401288 10.715034,0 19.401285,8.686251 19.401285,19.401288 z m 119.024218,0 c 0,10.71503 8.68625,19.40128 19.40128,19.40128 10.71504,0 19.40129,-8.68625 19.40129,-19.40128 0,-10.715037 -8.68625,-19.401288 -19.40129,-19.401288 -10.71503,0 -19.40128,8.686251 -19.40128,19.401288 z"
+          transform="matrix(0.34093532,0,0,0.34093532,-4.2053705,-2.9381011)" />
+        <path stroke="var(--accent-color)" style="stroke-width:3;"
+          d="M 7.7605272,25.970003 C 7.938346,23.657778 8.1161613,21.345599 9.0070007,19.502072 9.8978401,17.658545 11.501633,16.283818 13.105457,14.909063 M 59.97281,25.970003 C 59.794991,23.657778 59.617176,21.345599 58.726336,19.502072 57.835497,17.658545 56.231704,16.283818 54.62788,14.909063 M 7.7605272,41.763327 c 0.1778188,2.312225 0.3556341,4.624404 1.2464735,6.467931 0.8908394,1.843527 2.4946323,3.218255 4.0984563,4.593009 M 59.97281,41.763327 c -0.177819,2.312225 -0.355634,4.624404 -1.246474,6.467931 -0.890839,1.843527 -2.494632,3.218255 -4.098456,4.593009"/>
+        <path stroke="var(--accent-color)" style="stroke-width:3;"
+          d="m 26.458333,60.854167 c 2.469443,0.881944 4.938839,1.763871 7.408334,1.799414 2.469494,0.03554 4.938889,-0.7753 7.408333,-1.58616 M 26.458333,6.8791633 c 2.469443,-0.8819438 4.938839,-1.7638708 7.408334,-1.7994139 2.469494,-0.035543 4.938889,0.7753003 7.408333,1.5861599"/>
+      </svg>
+      <h2 class="service-name">Members</h2>
+    </div>
+    <hr>
+    <p>The following sites are in the ring! We're still new and always looking for members!</p>
+    <table>
+      <thead>
+        <tr>
+          <th>Website</th>
+          <th>NSFW?</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each data.webring as { website, nsfw }}
+        <tr>
+          <td><a href="https://{website}">{website}</a></td>
+          <td>{nsfw}</td>
+        </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+  <div class="service">
+    <div class="service-header">
+      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M6 6.207v9.043a.75.75 0 0 0 1.5 0V10.5a.5.5 0 0 1 1 0v4.75a.75.75 0 0 0 1.5 0v-8.5a.25.25 0 1 1 .5 0v2.5a.75.75 0 0 0 1.5 0V6.5a3 3 0 0 0-3-3H6.236a1 1 0 0 1-.447-.106l-.33-.165A.83.83 0 0 1 5 2.488V.75a.75.75 0 0 0-1.5 0v2.083c0 .715.404 1.37 1.044 1.689L5.5 5c.32.32.5.754.5 1.207"/>
+        <path d="M8 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+      </svg>
+      <h2 class="service-name">Join the Ring!</h2>
+    </div>
+    <hr>
+    <p>
+      If you already have a website, it's easy to join the ring! Just check the rules, then add the links: 
+    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Potential Label</th>
+          <th>href</th>
+        </tr>
+        <tr>
+          <td>ProtogenRing</td>
+          <td><code>https://protogen.chat/webring</code></td>
+        </tr>
+        <tr>
+          <td>&lAarr; Prev</td>
+          <td><code>https://protogen.chat/webring/<i>your.domain</i>/previous</code></td>
+        </tr>
+        <tr>
+          <td>Next &rAarr;</td>
+          <td><code>https://protogen.chat/webring/<i>your.domain</i>/next</code></td>
+        </tr>
+      </thead>
+    </table>
+    <p>
+      There's no set styling for the ring (yet!). Please check out members' sites for inspiration.
+    </p>
+    <p>
+      Once you're ready, <a href="/contact">send a message</a> or (if you're feeling savvy) 
+      <a href="https://github.com/HiiJax/protogen.chat/blob/main/src/routes/webring/webring.js">add yourself</a> and 
+      <a href="https://github.com/HiiJax/protogen.chat/pulls">open a pull request</a>!
+    </p>
+  </div>
+  <!--
+  <p>The following links are available to use, but not currently required to join the Protogen Webring.</p>
+  <table>
+    <thead>
+      <tr>
+        <th>Suggested Label</th>
+        <th>href</th>
+      </tr>
+      <tr>
+        <td>Random</td>
+        <td><code>https://protogen.chat/webring/<i>your.domain</i>/random</code></td>
+      </tr>
+    </thead>
+  </table>
+  -->
+  <div class="service">
+    <div class="service-header">
+      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"/>
+        <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+      </svg>
+      <h2 class="service-name">Rules</h2>
+    </div>
+    <hr>
+    <div class="rules">
+    <p>All rules are subject to change</p>
+    <ul>
+      <li>NSFW content MUST be gated, so people that don't wanna see it don't have to.</li>
+      <li>You don't necessarily need to be a protogen to join, you just need content protogen fans would enjoy (protogen art, suits, etc)</li>
+      <li>Hate is not tolerated.</li>
+      <li>Keeping furry weird is fine. But being rude, creepy, etc is not acceptable.</li>
+    </ul>
+    </div>
+    <p><strong>If you see any of these rules being broken, please <a href="/contact">contact Jax</a> ASAP.</strong></p>
+  </div>
+  <!--<div class="service">
+    <div class="service-header">
+      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9M3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708z"/>
+        <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"/>
+      </svg>
+      <h2 class="service-name">The Guts...</h2>
+    </div>
+    <hr>
+    <p>So you want to know about the inner workings of this ring? You nerd. You're cool, I like you.</p>
+    <p>
+      Good news for you is that all the code is <a href="https://github.com/HiiJax/protogen.chat/tree/main/src/routes/webring">on GitHub</a>.
+      It's free for you to use as you'd like, as long as you follow the <a href="https://github.com/HiiJax/protogen.chat/blob/main/LICENSE">MIT license</a>
+      (easy to do). It's totally custom made by Jax, without using any existing webring code or AI! It's made in <a href="https://svelte.dev/">SvelteKit</a>, 
+      which actually has a routing system that made it super easy to build. Everything done here can be learned from their rather solid set of tutorials.
+    </p>
+    <p>
+      With that being said, Jax is still a rather novice developer, so if you find something funky, please 
+      <a href="https://github.com/HiiJax/protogen.chat/issues">report it</a>!
+    </p>
+  </div>-->
 </div>
-
-<p><strong>If you see any of these rules being broken, please <a href="/contact">contact Jax</a> ASAP.</strong></p>
-<hr>
-<h2>The Guts...</h2>
-<p>So you want to know about the inner workings of this ring? You nerd. You're cool, I like you.</p>
-<p>
-  Good news for you is that all the code is <a href="https://github.com/HiiJax/protogen.chat/tree/main/src/routes/webring">on GitHub</a>.
-  It's free for you to use as you'd like, as long as you follow the <a href="https://github.com/HiiJax/protogen.chat/blob/main/LICENSE">MIT license</a>
-  (easy to do). It's totally custom made by Jax, without using any existing webring code or AI! It's made in <a href="https://svelte.dev/">SvelteKit</a>, 
-  which actually has a routing system that made it super easy to build. Everything done here can be learned from their rather solid set of tutorials.
-</p>
-<p>
-  With that being said, Jax is still a rather novice developer, so if you find something funky, please 
-  <a href="https://github.com/HiiJax/protogen.chat/issues">report it</a>!
-</p>
