d4d0251e492693eaaa6b3f85b53a94ac8a3b0567
Jax <git@hiijax.net>
2026-04-22 20:08:34 -0400

    adwaita icons + credits

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -19,11 +19,23 @@
   <li><a href="https://zoruniverse.info/">ZENITH'S OUTER REACH</a></li>
   <li>This website is not associated with or endorsed by KOINU or ZOR</li>
 </ul>
-<h3>Adwaita fonts </h3>
+<h3>Adwaita Fonts</h3>
 <ul>
   <li>&copy; 2016, <a href="https://github.com/rsms/inter">The Inter Project Authors</a></li>
   <li>&copy; 2015-2025, Renzhi Li (aka. Belleve Invis, belleve@typeof.net)</li>
   <li>&copy; 2025, <a href="https://jamie.garden">Jamie Gravendeel</a></li>
   <li>&copy; 2025, Florian Müllner</li>
+  <li><a href="https://gitlab.gnome.org/GNOME/adwaita-fonts">https://gitlab.gnome.org/GNOME/adwaita-fonts</a></li>
 </ul>
-<p><strong>Very inspired by <a href="https://synth.download">synth.download</a> 💛</strong></p>
+<h3>Adwaita Icons</h3>
+<ul>
+  <li>CC-BY-SA, <a href="http://www.gnome.org">GNOME Project</a></li>
+  <li><a href="https://gitlab.gnome.org/GNOME/adwaita-icon-theme">https://gitlab.gnome.org/GNOME/adwaita-icon-theme</a></li>
+</ul>
+<h3>Forgejo Logo</h3>
+<ul>
+  <li>CC-BY-SA, <a href="https://caesarschinas.com/">Caesar Schinas</a></li>
+  <li><a href="https://codeberg.org/forgejo/governance/src/branch/main/branding">https://codeberg.org/forgejo/governance/src/branch/main/branding</a></li>
+</ul>
+<br>
+<h2>Very inspired by <a href="https://synth.download">synth.download</a> 💛</h2>
