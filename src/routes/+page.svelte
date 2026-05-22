8a9d8bb94684d56aaaa682e9b255d17168383b7c
Jax <git@hiijax.net>
2026-05-22 15:47:29 -0400

    home page update, rules tweak, prettier did stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
   let rightEyeClose: SVGAnimateElement;
   let rightEyeOpen: SVGAnimateElement;
 
-  let protoMouth: HTMLElement
+  let protoMouth: HTMLElement;
 
   let leftEyeCount = 0;
   let rightEyeCount = 0;
@@ -14,10 +14,10 @@
   let hue = 0;
   let tCount = 0;
 
-  let bootSound: HTMLAudioElement
+  let bootSound: HTMLAudioElement;
 
   onMount(() => {
-    bootSound = new Audio('boot.opus');
+    bootSound = new Audio("boot.opus");
     protoMouth = document.getElementById("proto-mouth")!;
   });
 
@@ -27,7 +27,7 @@
 
   function mouth() {
     protoMouth.classList.remove("proto-mouth-anim");
-    setTimeout(mouthAnimStart, 50)
+    setTimeout(mouthAnimStart, 50);
     const sound = bootSound.cloneNode() as HTMLAudioElement;
     sound.play();
   }
@@ -58,25 +58,33 @@
   function prowogen() {
     tCount += 1;
     if (tCount == 10) {
-      const prowogen = document.getElementById('prowogen');
+      const prowogen = document.getElementById("prowogen");
       if (prowogen) {
-        prowogen.innerHTML = prowogen.innerHTML.replace('t','w');
-      } 
+        prowogen.innerHTML = prowogen.innerHTML.replace("t", "w");
+      }
       setInterval(() => {
         hue = (hue + 1) % 360;
-        document.documentElement.style.setProperty('--accent-color', `hsl(${hue}, 70%, 60%)`);
+        document.documentElement.style.setProperty(
+          "--accent-color",
+          `hsl(${hue}, 70%, 60%)`,
+        );
       }, 10);
     }
   }
-
 </script>
 
 <svelte:head>
-    <title>home | protogen.chat</title>
+  <title>home | protogen.chat</title>
 </svelte:head>
 
 <div class="hero">
-  <h1>Welcome, pro<span id="prowogen" role="button" tabindex="0" onkeydown={(e) => (e.key === "Enter" ? prowogen() : null)} onclick={prowogen}>t</span>ogens and the like!</h1>
+  welcome, pro<span
+    id="prowogen"
+    role="button"
+    tabindex="0"
+    onkeydown={(e) => (e.key === "Enter" ? prowogen() : null)}
+    onclick={prowogen}>t</span
+  >ogens and internet critters!
 </div>
 <div class="proto-face-holder flicker-in">
   <svg
@@ -84,91 +92,86 @@
     viewBox="226.32642 175.08014 827.34717 385.84607"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path id="proto-eye-left"
+    <path
+      id="proto-eye-left"
       onclick={leftEye}
-      role="button" 
+      role="button"
       tabindex="0"
-      onkeydown={(e) => (e.key === "Enter"  ? leftEye() : null)}
+      onkeydown={(e) => (e.key === "Enter" ? leftEye() : null)}
       style="fill:currentColor;stroke:currentColor;stroke-width:16.1197;stroke-linecap:butt;stroke-linejoin:round;"
-      d="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z">
-      <animate 
-        bind:this={leftEyeClose} 
+      d="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z"
+    >
+      <animate
+        bind:this={leftEyeClose}
         id="proto-eye-left-close"
         attributeName="d"
         begin="indefinite"
         dur="0.1s"
         from="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z"
-        to="m 234.38608,240.37577 c 24.5032,-16.5956 78.46731,99.64728 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z" 
-        fill="freeze"/>
-      <animate 
-        bind:this={leftEyeOpen} 
+        to="m 234.38608,240.37577 c 24.5032,-16.5956 78.46731,99.64728 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z"
+        fill="freeze"
+      />
+      <animate
+        bind:this={leftEyeOpen}
         id="proto-eye-left-close"
         attributeName="d"
         begin="indefinite"
         dur="0.1s"
         from="m 234.38608,240.37577 c 24.5032,-16.5956 78.46731,99.64728 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z"
-        to="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z" 
-        fill="freeze"/>
+        to="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z"
+        fill="freeze"
+      />
     </path>
-    <path id="proto-eye-right"
+    <path
+      id="proto-eye-right"
       onclick={rightEye}
-      role="button" 
+      role="button"
       tabindex="0"
-      onkeydown={(e) => (e.key === "Enter"  ? rightEye() : null)}
+      onkeydown={(e) => (e.key === "Enter" ? rightEye() : null)}
       style="fill:currentColor;stroke:currentColor;stroke-width:16.1197;stroke-linecap:butt;stroke-linejoin:round;"
-      d="m 1045.6139,240.37577 c -64.23543,-77.18025 -211.34693,-102.97062 -275.36407,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z">
-      <animate 
-        bind:this={rightEyeClose} 
+      d="m 1045.6139,240.37577 c -64.23543,-77.18025 -211.34693,-102.97062 -275.36407,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
+    >
+      <animate
+        bind:this={rightEyeClose}
         id="proto-eye-right-close"
         attributeName="d"
         begin="indefinite"
         dur="0.1s"
         from="m 1045.6139,240.37577 c -64.23543,-77.18025 -211.34693,-102.97062 -275.36407,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
-        to="m 1045.6139,240.37577 c -24.5032,-16.5956 -78.46734,99.64728 -275.36413,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z" 
-        fill="freeze"/>
-      <animate 
-        bind:this={rightEyeOpen} 
+        to="m 1045.6139,240.37577 c -24.5032,-16.5956 -78.46734,99.64728 -275.36413,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
+        fill="freeze"
+      />
+      <animate
+        bind:this={rightEyeOpen}
         id="proto-eye-right-open"
         attributeName="d"
         begin="indefinite"
         dur="0.1s"
-        from="m 1045.6139,240.37577 c -24.5032,-16.5956 -78.46734,99.64728 -275.36413,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z" 
+        from="m 1045.6139,240.37577 c -24.5032,-16.5956 -78.46734,99.64728 -275.36413,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
         to="m 1045.6139,240.37577 c -64.23543,-77.18025 -211.34693,-102.97062 -275.36407,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
-        fill="freeze"/>
+        fill="freeze"
+      />
     </path>
-    <path id="proto-mouth"
+    <path
+      id="proto-mouth"
       onclick={mouth}
-      role="button" 
+      role="button"
       tabindex="0"
-      onkeydown={(e) => (e.key === "Enter"  ? mouth() : null)}
+      onkeydown={(e) => (e.key === "Enter" ? mouth() : null)}
       style="fill:none;stroke:currentColor;stroke-width:12.7181;stroke-linecap:round;stroke-linejoin:round;"
-      d="m 284.2935,382.47733 84.4921,106.63523 c 12.19242,-26.46984 24.42329,-36.99579 40.2599,-47.02668 l 70.85543,102.48194 c 12.60287,-28.86446 29.57675,-48.70057 44.96206,-57.39729 19.36568,3.81975 91.88596,7.99763 116.16672,7.96716 m 354.6768,-112.66036 -84.4921,106.63523 c -12.19242,-26.46984 -24.42329,-36.99579 -40.2599,-47.02668 l -70.85543,102.48194 c -12.60287,-28.86446 -29.57675,-48.70057 -44.96206,-57.39729 -19.36568,3.81975 -91.88596,7.99763 -116.16672,7.96716"/>
-    <path id="proto-nose"
+      d="m 284.2935,382.47733 84.4921,106.63523 c 12.19242,-26.46984 24.42329,-36.99579 40.2599,-47.02668 l 70.85543,102.48194 c 12.60287,-28.86446 29.57675,-48.70057 44.96206,-57.39729 19.36568,3.81975 91.88596,7.99763 116.16672,7.96716 m 354.6768,-112.66036 -84.4921,106.63523 c -12.19242,-26.46984 -24.42329,-36.99579 -40.2599,-47.02668 l -70.85543,102.48194 c -12.60287,-28.86446 -29.57675,-48.70057 -44.96206,-57.39729 -19.36568,3.81975 -91.88596,7.99763 -116.16672,7.96716"
+    />
+    <path
+      id="proto-nose"
       onclick={nose}
-      role="button" 
+      role="button"
       tabindex="0"
-      onkeydown={(e) => (e.key === "Enter"  ? nose() : null)}
+      onkeydown={(e) => (e.key === "Enter" ? nose() : null)}
       style="fill:currentColor;stroke:currentColor;stroke-width:6.29291;stroke-linecap:round;stroke-linejoin:round;"
-      d="m 609.37335,397.44539 -15.39148,-13.41786 20.96793,2.36776 z m 61.25331,0 15.39148,-13.41786 -20.96793,2.36776 z"/>
+      d="m 609.37335,397.44539 -15.39148,-13.41786 20.96793,2.36776 z m 61.25331,0 15.39148,-13.41786 -20.96793,2.36776 z"
+    />
   </svg>
 </div>
-<div class="hero">
-  <div class="smol">
-    mess around with the face or choose one of the various links!
-  </div>
+<div class="hero-lower">
+  mess around with the face or choose one of the various links!
 </div>
-
-
-
-<!--
-  OLD BOTH EYE ANIMATION
-    <animate 
-        bind:this={bootAnim} 
-        id="proto-boot"
-        attributeName="d"
-        begin="indefinite"
-        dur="0.3s"
-        from="m 234.38608,240.37577 c 24.5032,-16.5956 78.46731,99.64728 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z m 811.22782,0 c -24.5032,-16.5956 -78.46734,99.64728 -275.36413,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
-        to="m 234.38608,240.37577 c 64.23546,-77.18025 211.34696,-102.97062 275.3641,96.70157 -132.97423,17.63342 -250.08281,-54.825 -275.3641,-96.70157 z m 811.22782,0 c -64.23543,-77.18025 -211.34693,-102.97062 -275.36407,96.70157 132.97423,17.63342 250.08277,-54.825 275.36407,-96.70157 z"
-        fill="freeze" />
--->
--- src/routes/webring/+page.svelte
+++ src/routes/webring/+page.svelte
@@ -1,43 +1,72 @@
 <script lang="ts">
-    let { data } = $props();
+  let { data } = $props();
 </script>
 
 <svelte:head>
-	<title>webring | protogen.chat</title>
+  <title>webring | protogen.chat</title>
 </svelte:head>
 
 <h1>Protogen Webring</h1>
-<a class="smol" href="https://indieweb.org/webring">First time seeing a webring? </a>
-<br>
-<br>
+<a class="smol" href="https://indieweb.org/webring"
+  >First time seeing a webring?
+</a>
+<br />
+<br />
 <div class="service-grid">
   <div class="service">
     <div class="service-header">
-      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 16 16" width="16px" fill="currentColor">
-        <path d="m 8 0 c -4.410156 0 -8 3.589844 -8 8 s 3.589844 8 8 8 s 8 -3.589844 8 -8 s -3.589844 -8 -8 -8 z m 0 2 c 3.332031 0 6 2.667969 6 6 s -2.667969 6 -6 6 s -6 -2.667969 -6 -6 s 2.667969 -6 6 -6 z m 0 1.875 c -0.621094 0 -1.125 0.503906 -1.125 1.125 s 0.503906 1.125 1.125 1.125 s 1.125 -0.503906 1.125 -1.125 s -0.503906 -1.125 -1.125 -1.125 z m -1.523438 3.125 c -0.265624 0.011719 -0.476562 0.230469 -0.476562 0.5 c 0 0.277344 0.222656 0.5 0.5 0.5 h 0.5 v 3 h -0.5 c -0.277344 0 -0.5 0.222656 -0.5 0.5 s 0.222656 0.5 0.5 0.5 h 3 c 0.277344 0 0.5 -0.222656 0.5 -0.5 s -0.222656 -0.5 -0.5 -0.5 h -0.5 v -4 h -2.5 c -0.007812 0 -0.015625 0 -0.023438 0 z m 0 0"/>
+      <svg
+        class="service-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="16px"
+        viewBox="0 0 16 16"
+        width="16px"
+        fill="currentColor"
+      >
+        <path
+          d="m 8 0 c -4.410156 0 -8 3.589844 -8 8 s 3.589844 8 8 8 s 8 -3.589844 8 -8 s -3.589844 -8 -8 -8 z m 0 2 c 3.332031 0 6 2.667969 6 6 s -2.667969 6 -6 6 s -6 -2.667969 -6 -6 s 2.667969 -6 6 -6 z m 0 1.875 c -0.621094 0 -1.125 0.503906 -1.125 1.125 s 0.503906 1.125 1.125 1.125 s 1.125 -0.503906 1.125 -1.125 s -0.503906 -1.125 -1.125 -1.125 z m -1.523438 3.125 c -0.265624 0.011719 -0.476562 0.230469 -0.476562 0.5 c 0 0.277344 0.222656 0.5 0.5 0.5 h 0.5 v 3 h -0.5 c -0.277344 0 -0.5 0.222656 -0.5 0.5 s 0.222656 0.5 0.5 0.5 h 3 c 0.277344 0 0.5 -0.222656 0.5 -0.5 s -0.222656 -0.5 -0.5 -0.5 h -0.5 v -4 h -2.5 c -0.007812 0 -0.015625 0 -0.023438 0 z m 0 0"
+        />
       </svg>
       <h2 class="service-name">About</h2>
     </div>
-    <hr>
+    <hr />
     <p>
-      Within this webring, you'll find websites related to "protogens", a fictional species created by 
-      <a href="https://coolkoinu.carrd.co/">KOINU</a> in the <a href="https://en.wikipedia.org/wiki/Furry_fandom">furry fandom</a>.
-      Members might use them to represent themselves (this is usually referred to as a <a href="https://en.wikipedia.org/wiki/Fursona">fursona</a>), 
-      have created character(s) based on the species, be an artist that draws other's protogens, or maybe they're just a really big fan of protogens!
+      Within this webring, you'll find websites related to "protogens", a
+      fictional species created by
+      <a href="https://coolkoinu.carrd.co/">KOINU</a> in the
+      <a href="https://en.wikipedia.org/wiki/Furry_fandom">furry fandom</a>.
+      Members might use them to represent themselves (this is usually referred
+      to as a <a href="https://en.wikipedia.org/wiki/Fursona">fursona</a>), have
+      created character(s) based on the species, be an artist that draws other's
+      protogens, or maybe they're just a really big fan of protogens!
     </p>
     <p>
-      You can learn more about protogens on <a href="https://zoruniverse.info/">Zenith's Outer Reach</a>.
+      You can learn more about protogens on <a href="https://zoruniverse.info/"
+        >Zenith's Outer Reach</a
+      >.
     </p>
   </div>
   <div class="service">
     <div class="service-header">
-      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 16 16" width="16px" fill="currentColor">
-        <path d="m 5 1 c -1.378906 0 -2.5 1.121094 -2.5 2.5 c 0 1.382812 1.121094 2.5 2.5 2.5 c 1.382812 0 2.5 -1.117188 2.5 -2.5 c 0 -1.378906 -1.117188 -2.5 -2.5 -2.5 z m 6 3 c -1.378906 0 -2.5 1.121094 -2.5 2.5 c 0 1.382812 1.121094 2.5 2.5 2.5 c 1.382812 0 2.5 -1.117188 2.5 -2.5 c 0 -1.378906 -1.117188 -2.5 -2.5 -2.5 z m -8 3 c -1.660156 0 -3 1.339844 -3 3 v 2 c 0 0.554688 0.445312 1 1 1 h 4.078125 c 0 -2.042969 1.578125 -3.734375 3.578125 -3.90625 c -0.589844 -0.535156 -0.984375 -1.257812 -1.109375 -2.042969 c -0.175781 -0.03125 -0.359375 -0.050781 -0.546875 -0.050781 z m 6 3 c -1.660156 0 -3 1.339844 -3 3 v 2 c 0 0.554688 0.445312 1 1 1 h 8 c 0.554688 0 1 -0.445312 1 -1 v -2 c 0 -1.660156 -1.335938 -3 -3 -3 z m 0 0"/>
+      <svg
+        class="service-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="16px"
+        viewBox="0 0 16 16"
+        width="16px"
+        fill="currentColor"
+      >
+        <path
+          d="m 5 1 c -1.378906 0 -2.5 1.121094 -2.5 2.5 c 0 1.382812 1.121094 2.5 2.5 2.5 c 1.382812 0 2.5 -1.117188 2.5 -2.5 c 0 -1.378906 -1.117188 -2.5 -2.5 -2.5 z m 6 3 c -1.378906 0 -2.5 1.121094 -2.5 2.5 c 0 1.382812 1.121094 2.5 2.5 2.5 c 1.382812 0 2.5 -1.117188 2.5 -2.5 c 0 -1.378906 -1.117188 -2.5 -2.5 -2.5 z m -8 3 c -1.660156 0 -3 1.339844 -3 3 v 2 c 0 0.554688 0.445312 1 1 1 h 4.078125 c 0 -2.042969 1.578125 -3.734375 3.578125 -3.90625 c -0.589844 -0.535156 -0.984375 -1.257812 -1.109375 -2.042969 c -0.175781 -0.03125 -0.359375 -0.050781 -0.546875 -0.050781 z m 6 3 c -1.660156 0 -3 1.339844 -3 3 v 2 c 0 0.554688 0.445312 1 1 1 h 8 c 0.554688 0 1 -0.445312 1 -1 v -2 c 0 -1.660156 -1.335938 -3 -3 -3 z m 0 0"
+        />
       </svg>
       <h2 class="service-name">Members</h2>
     </div>
-    <hr>
-    <p>The following sites are in the ring! We're still new and always looking for members!</p>
+    <hr />
+    <p>
+      The following sites are in the ring! We're still new and always looking
+      for members!
+    </p>
     <table>
       <thead>
         <tr>
@@ -47,26 +76,41 @@
       </thead>
       <tbody>
         {#each data.webring as { website, nsfw }}
-        <tr>
-          <td><a href="https://{website}">{website}</a></td>
-          <td>{nsfw}</td>
-        </tr>
+          <tr>
+            <td><a href="https://{website}">{website}</a></td>
+            <td>{nsfw}</td>
+          </tr>
         {/each}
       </tbody>
     </table>
   </div>
   <div class="service">
     <div class="service-header">
-      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 16 16" width="16px" fill="currentColor">
-        <path d="m 3.957031 5.988281 c -0.101562 0.003907 -0.207031 0.023438 -0.304687 0.058594 c -0.734375 0.109375 -2.652344 0.691406 -2.652344 3.953125 v 5 h 4 v -4.691406 c 0.101562 0.070312 0.203125 0.136718 0.304688 0.195312 c 0.578124 0.320313 1.15625 0.433594 1.695312 0.476563 v -0.980469 h 1 v -1 c -0.003906 0 -0.003906 0 -0.007812 0 c -0.777344 0.003906 -1.3125 -0.023438 -1.71875 -0.25 c -0.40625 -0.222656 -0.871094 -0.703125 -1.324219 -2.066406 c -0.136719 -0.429688 -0.542969 -0.714844 -0.992188 -0.695313 z m 0 0"/>
-        <path d="m 12 1 c -1.105469 0 -2 0.894531 -2 2 s 0.894531 2 2 2 s 2 -0.894531 2 -2 s -0.894531 -2 -2 -2 z m -0.042969 4.988281 c -0.414062 0.015625 -0.777343 0.292969 -0.90625 0.695313 c -0.453125 1.363281 -0.917969 1.84375 -1.324219 2.066406 c -0.40625 0.226562 -0.945312 0.253906 -1.722656 0.25 h -0.003906 v 1 h -1 v 0.980469 c 0.167969 0.011719 0.332031 0.015625 0.492188 0.015625 c 0.003906 0 0.003906 0.003906 0.007812 0.003906 h 0.492188 h 0.011718 h 0.496094 c 0.003906 0 0.003906 -0.003906 0.003906 -0.003906 c 0.675782 -0.007813 1.433594 -0.074219 2.1875 -0.492188 c 0.105469 -0.058594 0.207032 -0.125 0.308594 -0.195312 v 4.691406 h 4 v -5 c 0 -3.261719 -1.917969 -3.84375 -2.65625 -3.953125 c -0.097656 -0.035156 -0.199219 -0.054687 -0.304688 -0.058594 c -0.027343 -0.003906 -0.054687 -0.003906 -0.082031 0 z m 0 0" fill-opacity="0.501961"/>
-        <path d="m 4 1 c -1.105469 0 -2 0.894531 -2 2 s 0.894531 2 2 2 s 2 -0.894531 2 -2 s -0.894531 -2 -2 -2 z m 0 0"/>
+      <svg
+        class="service-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="16px"
+        viewBox="0 0 16 16"
+        width="16px"
+        fill="currentColor"
+      >
+        <path
+          d="m 3.957031 5.988281 c -0.101562 0.003907 -0.207031 0.023438 -0.304687 0.058594 c -0.734375 0.109375 -2.652344 0.691406 -2.652344 3.953125 v 5 h 4 v -4.691406 c 0.101562 0.070312 0.203125 0.136718 0.304688 0.195312 c 0.578124 0.320313 1.15625 0.433594 1.695312 0.476563 v -0.980469 h 1 v -1 c -0.003906 0 -0.003906 0 -0.007812 0 c -0.777344 0.003906 -1.3125 -0.023438 -1.71875 -0.25 c -0.40625 -0.222656 -0.871094 -0.703125 -1.324219 -2.066406 c -0.136719 -0.429688 -0.542969 -0.714844 -0.992188 -0.695313 z m 0 0"
+        />
+        <path
+          d="m 12 1 c -1.105469 0 -2 0.894531 -2 2 s 0.894531 2 2 2 s 2 -0.894531 2 -2 s -0.894531 -2 -2 -2 z m -0.042969 4.988281 c -0.414062 0.015625 -0.777343 0.292969 -0.90625 0.695313 c -0.453125 1.363281 -0.917969 1.84375 -1.324219 2.066406 c -0.40625 0.226562 -0.945312 0.253906 -1.722656 0.25 h -0.003906 v 1 h -1 v 0.980469 c 0.167969 0.011719 0.332031 0.015625 0.492188 0.015625 c 0.003906 0 0.003906 0.003906 0.007812 0.003906 h 0.492188 h 0.011718 h 0.496094 c 0.003906 0 0.003906 -0.003906 0.003906 -0.003906 c 0.675782 -0.007813 1.433594 -0.074219 2.1875 -0.492188 c 0.105469 -0.058594 0.207032 -0.125 0.308594 -0.195312 v 4.691406 h 4 v -5 c 0 -3.261719 -1.917969 -3.84375 -2.65625 -3.953125 c -0.097656 -0.035156 -0.199219 -0.054687 -0.304688 -0.058594 c -0.027343 -0.003906 -0.054687 -0.003906 -0.082031 0 z m 0 0"
+          fill-opacity="0.501961"
+        />
+        <path
+          d="m 4 1 c -1.105469 0 -2 0.894531 -2 2 s 0.894531 2 2 2 s 2 -0.894531 2 -2 s -0.894531 -2 -2 -2 z m 0 0"
+        />
       </svg>
       <h2 class="service-name">Join the Ring!</h2>
     </div>
-    <hr>
+    <hr />
     <p>
-      If you already have a website, it's easy to join the ring! Just check the rules, then add the links: 
+      If you already have a website, it's easy to join the ring! Just check the
+      rules, then add the links:
     </p>
     <table>
       <thead>
@@ -80,81 +124,89 @@
         </tr>
         <tr>
           <td>&lAarr; Prev</td>
-          <td><code>https://protogen.chat/webring/<i>your.domain</i>/previous</code></td>
+          <td
+            ><code
+              >https://protogen.chat/webring/<i>your.domain</i>/previous</code
+            ></td
+          >
         </tr>
         <tr>
           <td>Next &rAarr;</td>
-          <td><code>https://protogen.chat/webring/<i>your.domain</i>/next</code></td>
+          <td
+            ><code>https://protogen.chat/webring/<i>your.domain</i>/next</code
+            ></td
+          >
         </tr>
         <tr>
           <td>Random (optional)</td>
-          <td><code>https://protogen.chat/webring/<i>your.domain</i>/random</code></td>
+          <td
+            ><code>https://protogen.chat/webring/<i>your.domain</i>/random</code
+            ></td
+          >
         </tr>
       </thead>
     </table>
     <p>
-      There's no set styling for the ring (yet!). Please check out members' sites for inspiration.
+      There's no set styling for the ring (yet!). Please check out members'
+      sites for inspiration.
     </p>
     <p>
-      Once you're ready, <a href="/contact">send a message</a> or (if you're feeling savvy) 
-      <a href="https://github.com/HiiJax/protogen.chat/blob/main/src/routes/webring/webring.ts">add yourself</a> and 
-      <a href="https://github.com/HiiJax/protogen.chat/pulls">open a pull request</a>!
+      Once you're ready, <a href="/contact">send a message</a> or (if you're
+      feeling savvy)
+      <a
+        href="https://github.com/HiiJax/protogen.chat/blob/main/src/routes/webring/webring.ts"
+        >add yourself</a
+      >
+      and
+      <a href="https://github.com/HiiJax/protogen.chat/pulls"
+        >open a pull request</a
+      >!
     </p>
   </div>
-  <!--
-  <p>The following links are available to use, but not currently required to join the Protogen Webring.</p>
-  <table>
-    <thead>
-      <tr>
-        <th>Suggested Label</th>
-        <th>href</th>
-      </tr>
-      <tr>
-        <td>Random</td>
-        <td><code>https://protogen.chat/webring/<i>your.domain</i>/random</code></td>
-      </tr>
-    </thead>
-  </table>
-  -->
   <div class="service">
     <div class="service-header">
-      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 0 16 16" width="16px" fill="currentColor">
-        <path d="m 7.96875 5.957031 c 0.542969 -0.015625 1.046875 0.488281 1.03125 1.03125 v 1 c 0.007812 0.527344 -0.472656 1 -1 1 s -1.007812 -0.472656 -1 -1 v -1 c -0.007812 -0.464843 0.355469 -0.914062 0.8125 -1 c 0.050781 -0.015625 0.101562 -0.023437 0.15625 -0.03125 z m 0.03125 4.03125 c 0.550781 0 1 0.449219 1 1 s -0.449219 1 -1 1 s -1 -0.449219 -1 -1 s 0.449219 -1 1 -1 z m 0 0"/>
-        <path d="m 8 1.359375 c -0.769531 0 -1.535156 0.375 -1.941406 1.125 l -4.878906 9.0625 c -0.816407 1.515625 0.332031 3.441406 2.054687 3.441406 h 9.53125 c 1.722656 0 2.871094 -1.925781 2.054687 -3.441406 l -4.882812 -9.0625 c -0.402344 -0.75 -1.167969 -1.125 -1.9375 -1.125 z m -0.179688 2.070313 c 0.101563 -0.191407 0.257813 -0.191407 0.359376 0 l 4.878906 9.066406 c 0.144531 0.261718 0.003906 0.492187 -0.292969 0.492187 h -9.53125 c -0.296875 0 -0.4375 -0.230469 -0.296875 -0.492187 z m 0 0"/>
+      <svg
+        class="service-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        height="16px"
+        viewBox="0 0 16 16"
+        width="16px"
+        fill="currentColor"
+      >
+        <path
+          d="m 7.96875 5.957031 c 0.542969 -0.015625 1.046875 0.488281 1.03125 1.03125 v 1 c 0.007812 0.527344 -0.472656 1 -1 1 s -1.007812 -0.472656 -1 -1 v -1 c -0.007812 -0.464843 0.355469 -0.914062 0.8125 -1 c 0.050781 -0.015625 0.101562 -0.023437 0.15625 -0.03125 z m 0.03125 4.03125 c 0.550781 0 1 0.449219 1 1 s -0.449219 1 -1 1 s -1 -0.449219 -1 -1 s 0.449219 -1 1 -1 z m 0 0"
+        />
+        <path
+          d="m 8 1.359375 c -0.769531 0 -1.535156 0.375 -1.941406 1.125 l -4.878906 9.0625 c -0.816407 1.515625 0.332031 3.441406 2.054687 3.441406 h 9.53125 c 1.722656 0 2.871094 -1.925781 2.054687 -3.441406 l -4.882812 -9.0625 c -0.402344 -0.75 -1.167969 -1.125 -1.9375 -1.125 z m -0.179688 2.070313 c 0.101563 -0.191407 0.257813 -0.191407 0.359376 0 l 4.878906 9.066406 c 0.144531 0.261718 0.003906 0.492187 -0.292969 0.492187 h -9.53125 c -0.296875 0 -0.4375 -0.230469 -0.296875 -0.492187 z m 0 0"
+        />
       </svg>
       <h2 class="service-name">Rules</h2>
     </div>
-    <hr>
+    <hr />
     <div class="rules">
-    <p>All rules are subject to change</p>
-    <ul>
-      <li>18+ content should be gated (as in some kind of prior warning) so venturers of the ring aren't caught by surprise!</li>
-      <li>You don't necessarily need to be a protogen to join, just having content protogen fans would enjoy is great! (protogen art, suits, etc)</li>
-      <li>Hate, bigotry, and harmful acts are not tolerated.</li>
-      <li>Keeping furry weird is fine. But being rude, creepy, or ignoring boundaries is not acceptable.</li>
-    </ul>
+      <p>All rules are subject to change</p>
+      <ul>
+        <li>
+          You <i>should</i> have protogen content on your site, but really as long
+          as you're friendly with us toasters, we welcome you :3
+        </li>
+        <li>
+          18+ content should be gated (as in some kind of prior warning) so
+          venturers of the ring aren't caught by surprise!
+        </li>
+        <li>Hate, bigotry, and harmful acts are not tolerated.</li>
+        <li>
+          Keeping furry weird is fine. But being rude, creepy, or ignoring
+          boundaries is not acceptable.
+        </li>
+      </ul>
     </div>
-    <p><strong>If you see any of these rules being broken, please <a href="/contact">contact Jax</a> ASAP.</strong></p>
+    <p>
+      <strong
+        >If you see any of these rules being broken, please <a href="/contact"
+          >contact Jax</a
+        > ASAP.</strong
+      >
+    </p>
   </div>
-  <!--<div class="service">
-    <div class="service-header">
-      <svg class="service-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M6 9a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3A.5.5 0 0 1 6 9M3.854 4.146a.5.5 0 1 0-.708.708L4.793 6.5 3.146 8.146a.5.5 0 1 0 .708.708l2-2a.5.5 0 0 0 0-.708z"/>
-        <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1z"/>
-      </svg>
-      <h2 class="service-name">The Guts...</h2>
-    </div>
-    <hr>
-    <p>So you want to know about the inner workings of this ring? You nerd. You're cool, I like you.</p>
-    <p>
-      Good news for you is that all the code is <a href="https://github.com/HiiJax/protogen.chat/tree/main/src/routes/webring">on GitHub</a>.
-      It's free for you to use as you'd like, as long as you follow the <a href="https://github.com/HiiJax/protogen.chat/blob/main/LICENSE">MIT license</a>
-      (easy to do). It's totally custom made by Jax, without using any existing webring code or AI! It's made in <a href="https://svelte.dev/">SvelteKit</a>, 
-      which actually has a routing system that made it super easy to build. Everything done here can be learned from their rather solid set of tutorials.
-    </p>
-    <p>
-      With that being said, Jax is still a rather novice developer, so if you find something funky, please 
-      <a href="https://github.com/HiiJax/protogen.chat/issues">report it</a>!
-    </p>
-  </div>-->
 </div>
